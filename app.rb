e67d47e60cc81f0ca4ae77a1bd0e06a4741e403e
Simon <simon.conway@condaal.com>
2016-11-22 12:43:02 +0000

    Added a border to the picture

--- app.rb
+++ app.rb
@@ -23,5 +23,5 @@ end
 
 get'/cat' do
 
-  "<img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'>"
+"<img src='http://bit.ly/1eze8aE' style='border: #ff0000 4px dashed; padding-right: 2cm;' />"
 end
